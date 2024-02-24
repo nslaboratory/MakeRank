fbe5602ef5ff63727bde458015e20eddc8c73147
keita <cakeandkta@gmail.com>
2024-02-25 02:17:41 +0900

    add ios settings

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -56,6 +56,7 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
   final globalKey = GlobalKey();
   Uint8List? bytes;
+  bool isIOS = Platform.isIOS;
 
   Future _saveImage(GlobalKey globalKey) async {
     final boundary =
@@ -70,6 +71,169 @@ class _MyPageState extends State<MyPage> {
     await Share.shareXFiles([
       XFile.fromData(buffer, mimeType: "image/png"),
     ]);
+  }
+
+  Widget platformStack(bool isIOS) {
+    if (isIOS) {
+      print(isIOS);
+      return 
+          GridView.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 3.0,
+            mainAxisSpacing: 3.0,
+            padding: EdgeInsets.all(0.0),
+            shrinkWrap: true,
+            children: [
+              GridTile(
+                footer: GridTileBar(
+                  backgroundColor: Colors.black54,
+                  title: Text(
+                    widget.names[0],
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text("1位"),
+                ),
+                child: Image(
+                  image: widget.imgs[0]!.image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              GridTile(
+                footer: GridTileBar(
+                  backgroundColor: Colors.black54,
+                  title: Text(
+                    widget.names[1],
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text("2位"),
+                ),
+                child: Image(
+                  image: widget.imgs[1]!.image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              GridTile(
+                footer: GridTileBar(
+                  backgroundColor: Colors.black54,
+                  title: Text(
+                    widget.names[2],
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text("3位"),
+                ),
+                child: Image(
+                  image: widget.imgs[2]!.image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              GridTile(
+                footer: GridTileBar(
+                  backgroundColor: Colors.black54,
+                  title: Text(
+                    widget.names[3],
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text("4位"),
+                ),
+                child: Image(
+                  image: widget.imgs[3]!.image,
+                  fit: BoxFit.cover,
+                ),
+              )
+            ],
+          );
+    } else {
+      return Stack(
+        alignment: AlignmentDirectional.bottomEnd,
+        children: <Widget>[
+          GridView.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 3.0,
+            mainAxisSpacing: 3.0,
+            shrinkWrap: true,
+            children: [
+              GridTile(
+                footer: GridTileBar(
+                  backgroundColor: Colors.black54,
+                  title: Text(
+                    widget.names[0],
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text("1位"),
+                ),
+                child: Image(
+                  image: widget.imgs[0]!.image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              GridTile(
+                footer: GridTileBar(
+                  backgroundColor: Colors.black54,
+                  title: Text(
+                    widget.names[1],
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text("2位"),
+                ),
+                child: Image(
+                  image: widget.imgs[1]!.image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              GridTile(
+                footer: GridTileBar(
+                  backgroundColor: Colors.black54,
+                  title: Text(
+                    widget.names[2],
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text("3位"),
+                ),
+                child: Image(
+                  image: widget.imgs[2]!.image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              GridTile(
+                footer: GridTileBar(
+                  backgroundColor: Colors.black54,
+                  title: Text(
+                    widget.names[3],
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text("4位"),
+                ),
+                child: Image(
+                  image: widget.imgs[3]!.image,
+                  fit: BoxFit.cover,
+                ),
+              )
+            ],
+          ),
+          // Positioned(
+          //   bottom: 5,
+          //   right: 10,
+          //   child: Text("generated by Rankる", style: TextStyle(color: Colors.white),)
+          // )
+          // Align(
+          //   alignment: Alignment.bottomRight,
+          //   child: Padding(
+          //     padding: const EdgeInsets.all(2.0),
+          //     child: Text("generated by Rankる",
+          //         style: TextStyle(color: Colors.white)),
+          //   ),
+          // )
+        ],
+      );
+    }
   }
 
   @override
@@ -92,125 +256,122 @@ class _MyPageState extends State<MyPage> {
       ),
       backgroundColor: Color(0xFFF2F2F7),
       body: Center(
-        child: Padding(
-          //padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
-          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              RepaintBoundary(
-                key: globalKey,
-                child: Stack(
-                  //alignment: AlignmentDirectional.bottomEnd,
-                  alignment: AlignmentDirectional.center,
-                  children: <Widget>[
-                    GridView.count(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 3.0,
-                      mainAxisSpacing: 3.0,
-                      shrinkWrap: true,
-                      children: [
-                        GridTile(
-                          footer: GridTileBar(
-                            backgroundColor: Colors.black54,
-                            title: Text(
-                              widget.names[0],
-                              style: const TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                            subtitle: Text("1位"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RepaintBoundary(
+              key: globalKey,
+              //child: platformStack(isIOS),
+              child: Stack(
+                alignment: AlignmentDirectional.bottomEnd,
+                children: <Widget>[
+                  GridView.count(
+                    padding: EdgeInsets.all(0.0),
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 3.0,
+                    mainAxisSpacing: 3.0,
+                    shrinkWrap: true,
+                    children: [
+                      GridTile(
+                        footer: GridTileBar(
+                          backgroundColor: Colors.black54,
+                          title: Text(
+                            widget.names[0],
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
                           ),
-                          child: Image(
-                            image: widget.imgs[0]!.image,
-                            fit: BoxFit.cover,
-                          ),
+                          subtitle: Text("1位"),
                         ),
-                        GridTile(
-                          footer: GridTileBar(
-                            backgroundColor: Colors.black54,
-                            title: Text(
-                              widget.names[1],
-                              style: const TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                            subtitle: Text("2位"),
-                          ),
-                          child: Image(
-                            image: widget.imgs[1]!.image,
-                            fit: BoxFit.cover,
-                          ),
+                        child: Image(
+                          image: widget.imgs[0]!.image,
+                          fit: BoxFit.cover,
                         ),
-                        GridTile(
-                          footer: GridTileBar(
-                            backgroundColor: Colors.black54,
-                            title: Text(
-                              widget.names[2],
-                              style: const TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                            subtitle: Text("3位"),
+                      ),
+                      GridTile(
+                        footer: GridTileBar(
+                          backgroundColor: Colors.black54,
+                          title: Text(
+                            widget.names[1],
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
                           ),
-                          child: Image(
-                            image: widget.imgs[2]!.image,
-                            fit: BoxFit.cover,
-                          ),
+                          subtitle: Text("2位"),
                         ),
-                        GridTile(
-                          footer: GridTileBar(
-                            backgroundColor: Colors.black54,
-                            title: Text(
-                              widget.names[3],
-                              style: const TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                            subtitle: Text("4位"),
+                        child: Image(
+                          image: widget.imgs[1]!.image,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      GridTile(
+                        footer: GridTileBar(
+                          backgroundColor: Colors.black54,
+                          title: Text(
+                            widget.names[2],
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
                           ),
-                          child: Image(
-                            image: widget.imgs[3]!.image,
-                            fit: BoxFit.cover,
+                          subtitle: Text("3位"),
+                        ),
+                        child: Image(
+                          image: widget.imgs[2]!.image,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      GridTile(
+                        footer: GridTileBar(
+                          backgroundColor: Colors.black54,
+                          title: Text(
+                            widget.names[3],
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
                           ),
-                        )
-                      ],
-                    ),
-                    Positioned(
-                      bottom: 5,
-                      right: 10,
-                      child: Text("generated by Rankる", style: TextStyle(color: Colors.white),)
-                    )
-                    // Align(
-                    //   alignment: Alignment.bottomRight,
-                    //   child: Padding(
-                    //     padding: const EdgeInsets.all(2.0),
-                    //     child: Text("generated by Rankる",
-                    //         style: TextStyle(color: Colors.white)),
-                    //   ),
-                    // )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: MediaQuery.of(context).size.height * 0.06,
-                  child: FilledButton(
-                    style:
-                        FilledButton.styleFrom(shape: BeveledRectangleBorder()),
-                    child: const Text(
-                      "画像を保存 & シェア",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    onPressed: () {
-                      Fluttertoast.showToast(msg: "保存中...");
-                      print(widget.names);
-                      print(widget.imgs);
-                      _saveImage(globalKey);
-                    },
+                          subtitle: Text("4位"),
+                        ),
+                        child: Image(
+                          image: widget.imgs[3]!.image,
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                    ],
                   ),
+                  // Positioned(
+                  //   bottom: 5,
+                  //   right: 10,
+                  //   child: Text("generated by Rankる", style: TextStyle(color: Colors.white),)
+                  // )
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Text("generated by Rankる",
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.06,
+                child: FilledButton(
+                  style:
+                      FilledButton.styleFrom(shape: BeveledRectangleBorder()),
+                  child: const Text(
+                    "画像を保存 & シェア",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    Fluttertoast.showToast(msg: "保存中...");
+                    print(widget.names);
+                    print(widget.imgs);
+                    _saveImage(globalKey);
+                  },
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
